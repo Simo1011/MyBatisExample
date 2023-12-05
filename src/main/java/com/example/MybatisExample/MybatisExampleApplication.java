ec252766fd519bda33cb57b3a062c1c4ee93de3b
Mohamed Ait iaza <mohamedn1011@gmail.com>
2023-12-05 12:19:35 -0500

    added command line runner.

--- src/main/java/com/example/MybatisExample/MybatisExampleApplication.java
+++ src/main/java/com/example/MybatisExample/MybatisExampleApplication.java
@@ -1,13 +1,19 @@
 package com.example.MybatisExample;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MybatisExampleApplication {
+public class MybatisExampleApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+		System.out.println("main");
 		SpringApplication.run(MybatisExampleApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("run");
+	}
 }
